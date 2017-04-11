d9ec8e193692a071824cb91b658be69e14471dd3
LeiYaoUWaterloo <leiyao.uwaterloo@gmail.com>
2017-04-11 14:33:34 -0400

    start

---
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/project/ParseData.scala
@@ -0,0 +1,88 @@
+package ca.uwaterloo.cs.bigdata2017w.project
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import scala.collection.mutable._
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+import io.bespin.scala.util.Tokenizer
+
+class Conf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(review, output)
+  val review = opt[String](descr = "input path", required = true)
+  val output = opt[String](descr = "output path", required = true)
+  verify()
+}
+
+class Review {
+  @JsonProperty var review_id: String = null
+  @JsonProperty var user_id: String = null
+  @JsonProperty var business_id: String = null
+  @JsonProperty var stars: String = null
+  @JsonProperty var date: String = null
+  @JsonProperty var text: String = null
+  @JsonProperty var useful: String = null
+  @JsonProperty var funny: String = null
+  @JsonProperty var cool: String = null
+
+  override def toString = s"Review(user_id=$user_id, business_id=$business_id, stars=$stars, date=$date, text=$text, useful=$useful, funny=$funny, cool=$cool)"
+}
+
+object ParseData extends Tokenizer{
+  val log = Logger.getLogger(getClass().getName())
+
+  def wcIter(iter: Iterator[String]): Iterator[(String, Int)] = {
+    val counts = new HashMap[String, Int]() { override def default(key: String) = 0 }
+
+    iter.flatMap(line => tokenize(line))
+      .foreach { t => counts.put(t, counts(t) + 1) }
+
+    counts.iterator
+  }
+
+  val mapper = new ObjectMapper()
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  mapper.registerModule(DefaultScalaModule)
+
+  def main(argv: Array[String]): Unit = {
+    val args = new Conf(argv)
+
+    log.info("Input: " + args.review())
+    log.info("Output: " + args.output())
+
+    val conf = new SparkConf().setAppName("ParseData")
+    val sc = new SparkContext(conf)
+
+    val outputDir = new Path(args.output())
+    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+
+    val stop_word = sc.textFile("data/stopwords.txt")
+    val map = stop_word.map(line =>{
+      (line, 1)
+    }).collectAsMap()
+    val hashmap = sc.broadcast(map)
+
+    val review = sc.textFile(args.review())
+    var results = review.flatMap(record => {
+      Some(mapper.readValue(record, classOf[Review]))
+    }).map(array => {
+      array.text
+    }).flatMap(line => {
+      tokenize(line)
+    }).filter(word => {
+      !hashmap.value.contains(word)
+    }).mapPartitions(wcIter)
+      .reduceByKey(_ + _)
+      .map(tuple => tuple.swap)
+      .sortByKey(false)
+      .map(tuple => tuple.swap)
+      .take(150).foreach(println(_))
+  }
+}
